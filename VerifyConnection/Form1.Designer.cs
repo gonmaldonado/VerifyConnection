802f3ff7154b33a4f1b0ad2d7ddda0ee40142791
Gonzalo Maldonado <gmaldonado@ucema.edu.ar>
2023-06-11 18:59:31 -0300

    Init

---
+++ VerifyConnection/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboBD = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtConnectionstring = new System.Windows.Forms.TextBox();
+            this.btnTestConexion = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtConnectionstring, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnTestConexion, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.71428F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 47F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(426, 328);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.cboBD);
+            this.flowLayoutPanel1.Controls.Add(this.label3);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(420, 52);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Base de datos:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cboBD
+            // 
+            this.cboBD.FormattingEnabled = true;
+            this.cboBD.Items.AddRange(new object[] {
+            "",
+            "ORACLE",
+            "MySQL",
+            "SQL Server"});
+            this.cboBD.Location = new System.Drawing.Point(107, 3);
+            this.cboBD.Name = "cboBD";
+            this.cboBD.Size = new System.Drawing.Size(285, 24);
+            this.cboBD.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Connectionstring:";
+            // 
+            // txtConnectionstring
+            // 
+            this.txtConnectionstring.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtConnectionstring.Location = new System.Drawing.Point(3, 61);
+            this.txtConnectionstring.Multiline = true;
+            this.txtConnectionstring.Name = "txtConnectionstring";
+            this.txtConnectionstring.Size = new System.Drawing.Size(420, 216);
+            this.txtConnectionstring.TabIndex = 1;
+            // 
+            // btnTestConexion
+            // 
+            this.btnTestConexion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnTestConexion.Location = new System.Drawing.Point(3, 283);
+            this.btnTestConexion.Name = "btnTestConexion";
+            this.btnTestConexion.Size = new System.Drawing.Size(420, 42);
+            this.btnTestConexion.TabIndex = 2;
+            this.btnTestConexion.Text = "Conexion";
+            this.btnTestConexion.UseVisualStyleBackColor = true;
+            this.btnTestConexion.Click += new System.EventHandler(this.btnTestConexion_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 328);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VerifyConnection";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboBD;
+        private System.Windows.Forms.TextBox txtConnectionstring;
+        private System.Windows.Forms.Button btnTestConexion;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ VerifyConnection/Form1.cs
@@ -0,0 +1,113 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.OracleClient;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        public string strConexion = string.Empty;
+        public string mensaje = string.Empty;
+        public static string titulo = "VerifyConnection";
+        public bool MySQl()
+        {
+            MySqlConnection cnn;
+            try
+            {
+                cnn = new MySqlConnection(strConexion);
+                cnn.Open();
+                cnn.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+                return false;
+            }
+        }
+        public bool SQLServer()
+        {
+            SqlConnection cnn;           
+            try
+            {
+                cnn = new SqlConnection(strConexion);
+                cnn.Open();
+                cnn.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+                return false;
+            }
+        }
+        public bool Oracle()
+        {
+            OracleConnection cnn;      
+            try
+            {
+                cnn = new OracleConnection(strConexion);
+                cnn.Open();
+                cnn.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+                return false;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnTestConexion_Click(object sender, EventArgs e)
+        {
+            strConexion = txtConnectionstring.Text.Trim();
+            string bd = cboBD.Text.Trim();
+            if (!string.IsNullOrEmpty(strConexion) && !string.IsNullOrEmpty(bd))
+            {
+                switch (bd)
+                {
+                    case "ORACLE":
+                        mensaje = (Oracle()) ? "Conexion Exitosa" : mensaje;
+                        MessageBox.Show(mensaje, titulo);
+                        break;
+                    case "MySQL":
+                        mensaje = (MySQl()) ? "Conexion Exitosa" : mensaje;
+                        MessageBox.Show(mensaje, titulo);
+                        break;
+                    case "SQL Server":
+                        mensaje = (SQLServer()) ? "Conexion Exitosa" : mensaje;
+                        MessageBox.Show(mensaje, titulo);
+                        break;
+                    default:
+                        
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Complete todos los campos", titulo);
+            }
+
+        }
+    }
+}
